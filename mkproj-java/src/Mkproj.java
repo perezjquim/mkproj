7f08585252d401a8c4c1b857a3e76ae00def5eca
Joaquim Perez <jquim1@hotmail.com>
2018-01-28 16:39:23 +0000

    Templates more flexible to mess around with

--- mkproj-java/src/Mkproj.java
+++ mkproj-java/src/Mkproj.java
@@ -14,15 +14,36 @@ public class Mkproj
 	/* FIELD FOR THE PROJECT NAME */
 	private static TextField name;
 	
-	/* SELECTABLE PROGRAMMING LANGUAGES */
-	private static final RadioButton[] radios =
+	private static final Template[] languages =
 												{
-													new RadioButton("Java"),
-													new RadioButton("C"),
-													new RadioButton("Python"),
-													new RadioButton("Prolog"),
-													new RadioButton("Node.js")
+													new Template("c","C",".c",
+														"#include <stdio.h>\n"+
+														"#include <stdlib.h>\n"+
+														"#include <unistd.h>\n"+
+														"\n"+
+														"int main()\n"+
+														"{\n"+
+														"	printf(\"Great success\\n\");\n"+
+														"	return 0;\n"+
+														"}"),
+													new Template("java","Java",".java",
+														"package projName;\n"+
+														"\n"+
+														"public class projName\n"+
+														"{\n"+
+														"	public static void main(String[] args)\n"+
+														"	{\n"+
+														"		System.out.println(\"Great success\");\n"+
+														"	}\n"+
+														"}\n"),
+													new Template("nodejs","Node.js",".js",
+														"console.log(\"Great success\");"),
+													new Template("python","Python",".py",
+														"print \"Great success\";"),
+													new Template("prolog","Prolog",".pl",
+														"projName:-writeln('Great success').")
 												};
+	private static RadioButton[] radios;
 				
 	/* MAIN FUNCTION */									 
 	public static void main(String[] args)
@@ -41,7 +62,10 @@ public class Mkproj
 		Panel panLang = new Panel("Programming language");
 		
 		// The options/radio buttons are added to the panel
-		panLang.addButtonGroup(options);									
+		radios = new RadioButton[languages.length];
+		for(int i = 0; i < languages.length; i++)
+			radios[i] = new RadioButton(languages[i].getLabel());
+		panLang.addButtonGroup(radios);									
 		main.add(panLang);
 		
 		/* PANEL 'GENERATE PROJECT' */
@@ -76,7 +100,7 @@ public class Mkproj
 		else
 		{
 			// Gets the selected language
-			String lang = getSelectedOption();
+			Template lang = getSelectedLanguage();
 			
 			// If there was no language selected
 			if(lang == null)
@@ -98,117 +122,24 @@ public class Mkproj
 				// The project directory is created
 				Cmd.exec("mkdir "+projName,destination);
 				
-				switch(lang)
-				{
-					/* C */
-					case "C":
-						// The makefile is generated
-						Cmd.exec("mkmake c",new File(folderPath));
-						
-						// The file extension is added
-						srcPath += ".c";
-						
-						// The generic source code is written
-						IO.writeFile(srcPath,
-											"#include <stdio.h>\n"+
-											"#include <stdlib.h>\n"+
-											"#include <unistd.h>\n"+
-											"\n"+
-											"int main()\n"+
-											"{\n"+
-											"	printf(\"Great success\\n\");\n"+
-											"	return 0;\n"+
-											"}");
-											
-						// Popup message
-						IO.popup("C project generated successfully");
-						break;
-						
-					/* JAVA */
-					case "Java":
-						// The makefile is generated
-						Cmd.exec("mkmake java",new File(folderPath));
-						
-						// The file extension is added
-						srcPath += ".java";
-						
-						// The generic source code is written
-						IO.writeFile(srcPath,
-											"package "+projName+";\n"+
-											"\n"+
-											"public class "+projName+"\n"+
-											"{\n"+
-											"	public static void main(String[] args)\n"+
-											"	{\n"+
-											"		System.out.println(\"Great success\");\n"+
-											"	}\n"+
-											"}\n");
-											
-						// Popup message
-						IO.popup("Java project generated successfully");
-						break;
-						
-					/* NODE.JS */
-					case "Node.js":
-						// The makefile is generated
-						Cmd.exec("mkmake nodejs",new File(folderPath));
-						
-						// The file extension is added
-						srcPath += ".js";
-						
-						// The generic source code is written
-						IO.writeFile(srcPath,
-											"console.log(\"Great success\")");
-											
-						// Popup message
-						IO.popup("Node.js project generated successfully");
-						break;
-						
-					/* PYTHON */
-					case "Python":
-						// The makefile is generated
-						Cmd.exec("mkmake python",new File(folderPath));
-						
-						// The file extension is added
-						srcPath += ".py";
-						
-						// The generic source code is written
-						IO.writeFile(srcPath,
-											"print \"Great success\";");
-											
-						// Popup message
-						IO.popup("Python project generated successfully");
-						break;
-						
-					/* PROLOG */
-					case "Prolog":
-						// The makefile is generated
-						Cmd.exec("mkmake prolog",new File(folderPath));
-						
-						// The file extension is added
-						srcPath += ".pl";
-						
-						// The generic source code is written
-						IO.writeFile(srcPath,
-											projName+":-writeln('Great success').");
-											
-						// Popup message
-						IO.popup("Prolog project generated successfully");
-						break;
-				}			
+				lang.adaptCode(projName);
+				Cmd.exec("mkmake "+lang.getName(),new File(folderPath));
+				srcPath += lang.getSrcExtension();
+				IO.writeFile(srcPath,lang.getSrcCode());
+				IO.popup(lang.getLabel()+" project generated successfully");	
 			}
 		}		
 	}
 	
 	/* GETS THE SELECTED LANGUAGE */
-	public static String getSelectedOption()
+	public static Template getSelectedLanguage()
 	{
 		// Loop through all the options
-		for(RadioButton r : options)
+		for(int i = 0; i < radios.length; i++)
 		{
 			// If found a selected one
-			if(r.isSelected()) 
-				return r.getText();
+			if(radios[i].isSelected()) 
+				return languages[i];
 		}
 		
 		// In case nothing was selected
---
+++ mkproj-java/src/Template.java
@@ -0,0 +1,25 @@
+package mkproj;
+
+public class Template
+{
+	private String name;
+	private String label;
+	private String srcCode;
+	private String srcExtension;
+	
+	public Template(String name, String label, String srcExtension, String srcCode)
+	{
+		this.name = name;
+		this.label = label;
+		this.srcExtension = srcExtension;
+		this.srcCode = srcCode;
+	}
+	
+	public String getName() { return name; }
+	public String getLabel() { return label; }
+	public String getSrcCode() { return srcCode; }
+	public String getSrcExtension() { return srcExtension; }
+	
+	public void adaptCode(String projName)
+	{ srcCode.replace("projName",projName); }
+}
